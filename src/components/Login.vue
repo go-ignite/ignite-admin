a812c7fa88237b9b0344363193ed5e6bda498b8a
Timothy <yexiaozhou2003@gmail.com>
2017-06-19 17:54:19 +0800

    add login event handler

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,25 +2,34 @@
     <div>
         <h1 class="title">User Login</h1>
         <b-field label="Name">
-            <b-input value="John Silver"></b-input>
+            <b-input v-model="username"></b-input>
         </b-field>
         <b-field label="Password">
-            <b-input type="password" password-reveal>
+            <b-input type="password" password-reveal v-model="password">
             </b-input>
         </b-field>
+        <a class="button is-primary" @click="onLogin">Login</a>
     </div>
 </template>
 
 <script>
 module.exports = {
     data: function () {
-        return {}
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        onLogin: function (event) {
+            console.log('Username:' + this.username + ' Password:' + this.password);
+        }
     }
 }
 </script>
 
 <style>
 #login {
-  margin: 180px auto 180px auto;
+    margin: 170px auto 180px auto;
 }
 </style>
