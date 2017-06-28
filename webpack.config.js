99589ba2c141d8d4adc3a6500627ae7c35e0cab8
Wendell Sun <iwendellsun@gmail.com>
2017-06-28 19:36:07 +0800

    Temp commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+var path = require('path'), 
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+   webpack = require('webpack');
+
+
 module.exports = {
     entry: {
         index: __dirname + "/src/index.js",
         status: __dirname + "/src/main.js",
     },
     output: {
-        path: __dirname + "/static/js",
-        filename: "[name].bundle.js"
+					   path: path.resolve(__dirname, './static'),
+        filename: "js/[name].bundle.js"
     },
 
+
     module: {
-        loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
+         rules: [
+									   {
+              test: /\.css$/,
+									     use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+              })
+            },
+												{
+            test: /\.(jpg|png)$/,
+            use: [
+                'url-loader?limit=1&name=./images/[name].[ext]'
+              ]
+            },
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
@@ -30,5 +49,22 @@ module.exports = {
 
     plugins: [
         // new UglifyJSPlugin(),
+        new HtmlWebpackPlugin({
+            template: './templates/index.html',
+            filename: 'index.html'
+									}),
+        new HtmlWebpackPlugin({
+            template: './templates/code.html',
+            filename: 'code.html'
+									}),
+        new HtmlWebpackPlugin({
+            template: './templates/status.html',
+            filename: 'status.html'
+									}),
+        new HtmlWebpackPlugin({
+            template: './templates/users.html',
+            filename: 'users.html'
+									}),
+        new ExtractTextPlugin("css/style.css"),
     ]
 }
