b37c8f2f6df6df170a6619a546e2845478fb21b0
Timothy <yexiaozhou2003@gmail.com>
2017-06-29 17:16:33 +0800

    add input limit

--- src/components/ModalForm.vue
+++ src/components/ModalForm.vue
@@ -1,42 +1,33 @@
 <template>
     <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">批量生成邀请码</p>
-            </header>
-            <section class="modal-card-body">
-                <b-field label="数量">
-                    <b-input
-                        v-model="amount"
-                        placeholder="输入邀请码数量"
-                        required>
-                    </b-input>
-                </b-field>
-
-                <b-field label="流量 (GB)">
-                    <b-input
-                        v-model="limit"
-                        placeholder="输入月流量"
-                        required>
-                    </b-input>
-                </b-field>
-
-                <b-field label="有效期 (月)">
-                    <b-input
-                        v-model="available"
-                        placeholder="输入有效月数"
-                        required>
-                    </b-input>
-                </b-field>
-            </section>
-            <footer class="modal-card-foot">
-                <button class="button" type="button" @click="$emit('close')">关闭</button>
-                <button class="button is-primary" type="button" @click="onSubmit">批量生成</button>
-            </footer>
+        <header class="modal-card-head">
+            <p class="modal-card-title">批量生成邀请码</p>
+        </header>
+        <section class="modal-card-body">
+            <b-field label="数量">
+                <b-input v-model="amount" placeholder="输入邀请码数量" type="number" min="1" max="100" required>
+                </b-input>
+            </b-field>
+    
+            <b-field label="流量 (GB)">
+                <b-input v-model="limit" placeholder="输入月流量" type="number" min="1" max="999999" required>
+                </b-input>
+            </b-field>
+    
+            <b-field label="有效期 (月)">
+                <b-input v-model="available" placeholder="输入有效月数" type="number" min="1" max="999999" required>
+                </b-input>
+            </b-field>
+        </section>
+        <footer class="modal-card-foot">
+            <button class="button" type="button" @click="$emit('close')">关闭</button>
+            <button class="button is-primary" type="button" @click="onSubmit">批量生成</button>
+        </footer>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
 
     data() {
         return {
@@ -45,19 +36,19 @@
             available: 1,
         }
     },
-        props: ['refresh'],
-        methods: {
-            onSubmit() {
-                console.log("submit button clicked...");
-                this.$emit('close');
-            }
+    props: ['refresh'],
+    methods: {
+        onSubmit() {
+            console.log("submit button clicked...");
+            this.$emit('close');
         }
     }
+}
 </script>
 
 <style scoped>
-    .modal-card {
-        margin: 0 auto;
-        width: auto;
-    }
+.modal-card {
+    margin: 0 auto;
+    width: auto;
+}
 </style>
