d6267ee838d105dcebcaf9a17bbf453394df02f3
Wendell Sun <iwendellsun@gmail.com>
2018-01-23 16:49:07 +0800

    Check container status before traffic statistics

--- jobs/instant_stats.go
+++ jobs/instant_stats.go
@@ -25,7 +25,7 @@ func SetDB(engine *xorm.Engine) {
 func InstantStats() {
 	// 1. Load all service from user
 	users := []models.User{}
-	err := db.Where("service_id != '' AND status = 1").Find(&users)
+	err := db.Where("service_id != ''").Find(&users)
 	if err != nil {
 		log.Println("Get users error: ", err.Error())
 		return
@@ -33,52 +33,56 @@ func InstantStats() {
 
 	// 2. Compute ss bandwidth
 	for _, user := range users {
-		raw, err := ss.GetContainerStatsOutNet(user.ServiceId)
-		if err != nil {
-			log.Printf("Get container(%s) net out error: %s\n", user.ServiceId, err.Error())
-			continue
-		}
-
-		// Get container start time
-		startTime, err := ss.GetContainerStartTime(user.ServiceId)
-		if err != nil {
-			log.Printf("Get container(%s) start time error: %s\n", user.ServiceId, err.Error())
-			continue
-		}
-
-		// Update user package used
-		var bandwidth float32
-		if user.LastStatsTime == nil || user.LastStatsTime.Before(*startTime) {
-			bandwidth = float32(float64(raw) / GB)
-		} else {
-			bandwidth = float32(float64(raw-user.LastStatsResult) / GB)
-		}
-		user.PackageUsed += bandwidth
-
-		if int(user.PackageUsed) >= user.PackageLimit {
-			// Stop container && update user status
-			err := ss.StopContainer(user.ServiceId)
+		if ss.IsContainerRunning(user.ServiceId) {
+			user.Status = 1
+			raw, err := ss.GetContainerStatsOutNet(user.ServiceId)
 			if err != nil {
-				log.Printf("Stop container(%s) error: %s\n", user.ServiceId, err.Error())
-			} else {
-				log.Printf("STOP: user(%d-%s)-container(%s)\n", user.Id, user.Username, user.ServiceId[:12])
-				user.Status = 2
-				user.PackageUsed = float32(user.PackageLimit)
+				log.Printf("Get container(%s) net out error: %s\n", user.ServiceId, err.Error())
+				continue
 			}
+
+			// Get container start time
+			startTime, err := ss.GetContainerStartTime(user.ServiceId)
+			if err != nil {
+				log.Printf("Get container(%s) start time error: %s\n", user.ServiceId, err.Error())
+				continue
+			}
+
+			// Update user package used
+			var bandwidth float32
+			if user.LastStatsTime == nil || user.LastStatsTime.Before(*startTime) {
+				bandwidth = float32(float64(raw) / GB)
+			} else {
+				bandwidth = float32(float64(raw-user.LastStatsResult) / GB)
+			}
+			user.PackageUsed += bandwidth
+
+			if int(user.PackageUsed) >= user.PackageLimit {
+				// Stop container && update user status
+				err := ss.StopContainer(user.ServiceId)
+				if err != nil {
+					log.Printf("Stop container(%s) error: %s\n", user.ServiceId, err.Error())
+				} else {
+					log.Printf("STOP: user(%d-%s)-container(%s)\n", user.Id, user.Username, user.ServiceId[:12])
+					user.Status = 2
+					user.PackageUsed = float32(user.PackageLimit)
+				}
+			}
+			now := time.Now()
+			user.LastStatsResult = raw
+			user.LastStatsTime = &now
+			if bandwidth > 0 {
+				log.Printf("STATS: user(%d-%s)-container(%s)-bandwidth(%.2f)\n", user.Id, user.Username, user.ServiceId[:12], bandwidth)
+			}
+		} else {
+			user.Status = 2
 		}
 
 		// 3. Update user stats info
-		now := time.Now()
-		user.LastStatsTime = &now
-		user.LastStatsResult = raw
 		_, err = db.Id(user.Id).Cols("package_used", "last_stats_result", "last_stats_time", "status").Update(user)
 		if err != nil {
 			log.Printf("Update user(%d) error: %s\n", user.Id, err.Error())
 			continue
-		}
-
-		if bandwidth > 0 {
-			log.Printf("STATS: user(%d-%s)-container(%s)-bandwidth(%.2f)\n", user.Id, user.Username, user.ServiceId[:12], bandwidth)
 		}
 	}
 }
