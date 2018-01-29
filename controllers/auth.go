c06bff11b5bd9ac87a4139bae673d198d32a7421
Wendell Sun <iwendellsun@gmail.com>
2018-01-29 15:25:54 +0800

    Update renew handler

--- controllers/auth.go
+++ controllers/auth.go
@@ -134,10 +134,10 @@ func (router *MainRouter) RenewServiceHandler(c *gin.Context) {
 	}
 
 	renewEntity := struct {
-		Month int `json:"month"`
+		Expired int64 `json:"expired"`
 	}{}
 
-	if err := c.BindJSON(&renewEntity); err != nil || renewEntity.Month == 0 {
+	if err := c.BindJSON(&renewEntity); err != nil || renewEntity.Expired == 0 {
 		resp := models.Response{Success: false, Message: "续费参数不正确!"}
 		c.JSON(http.StatusOK, &resp)
 		return
@@ -151,34 +151,24 @@ func (router *MainRouter) RenewServiceHandler(c *gin.Context) {
 		return
 	}
 
-	user.Expired = user.Expired.AddDate(0, renewEntity.Month, 0)
-	if user.Expired.Before(user.Created) {
-		resp := models.Response{Success: false, Message: "服务到期时间不能早于创建时间!"}
+	expired, now := time.Unix(renewEntity.Expired, 0), time.Now()
+	if expired.Before(now) {
+		resp := models.Response{Success: false, Message: "服务到期时间不能早于当前时间!"}
 		c.JSON(http.StatusOK, &resp)
 		return
 	}
 
 	if user.ServiceId != "" {
-		var err error
-		running := ss.IsContainerRunning(user.ServiceId)
-		now := time.Now()
-		if running && user.Expired.Before(now) {
-			err = ss.StopContainer(user.ServiceId)
-			if err == nil {
-				user.Status = 2
+		if user.Expired.Before(now) && (user.Status == 2 || !ss.IsContainerRunning(user.ServiceId)) {
+			if err := ss.StartContainer(user.ServiceId); err != nil {
+				resp := models.Response{Success: false, Message: "启动服务失败!"}
+				c.JSON(http.StatusOK, &resp)
+				return
 			}
-		} else if !running && user.Expired.After(now) {
-			err = ss.StartContainer(user.ServiceId)
-			if err == nil {
-				user.Status = 1
-			}
-		}
-		if err != nil {
-			resp := models.Response{Success: false, Message: "启动/停止服务失败!"}
-			c.JSON(http.StatusOK, &resp)
-			return
+			user.Status = 1
 		}
 	}
+	user.Expired = expired
 
 	if _, err := router.db.Id(uid).Cols("expired", "status").Update(user); err != nil {
 		resp := models.Response{Success: false, Message: "更新过期时间失败!"}
