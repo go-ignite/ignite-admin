d70b89fcb36ba3d5e14d53a4bd193a40c539766f
Timothy <yexiaozhou2003@gmail.com>
2018-01-29 14:20:54 +0800

    add renew modal dialog

---
+++ src/components/RenewForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="modal-card">
+        <header class="modal-card-head">
+            <p class="modal-card-title">帐号续期</p>
+        </header>
+        <section class="modal-card-body">
+            <!-- <b-datepicker v-model="date" inline></b-datepicker> -->
+            <b-field label="Select a date">
+        <b-datepicker
+            placeholder="Click to select..."
+            icon="calendar-today">
+        </b-datepicker>
+    </b-field>
+        </section>
+        <footer class="modal-card-foot">
+            <button class="button" type="button" @click="$emit('close')">关闭</button>
+            <button class="button is-primary" type="button" @click="onSubmit">更新</button>
+        </footer>
+    </div>
+</template>
+
+<script>
+import request from '../apis/request';
+
+export default {
+    data() {
+        return {
+            date: new Date()
+        }
+    },
+    methods: {
+        onSubmit() {
+            let self = this;
+            request.post("/api/auth/code_generate", {
+                "amount": self.amount,
+                "limit": self.limit,
+                "available": self.available,
+            }).then((response) => {
+                self.$toast.open('生成邀请码成功!');
+                self.$emit('close');
+            }).catch((error) => {
+                self.$toast.open({
+                    message: '生成邀请码失败!',
+                    type: 'is-danger'
+                })
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+.modal-card {
+    margin: 0 auto;
+    width: auto;
+}
+</style>
