6324c7b7fedeb9e7c2b1095c6db98c20bb223e95
vv13 <zwhvv13@gmail.com>
2018-04-30 15:09:07 +0800

    refactor: use vue-cli@3.0 to build fe.

--- fe/src/main.js
+++ fe/src/main.js
@@ -1,16 +1,15 @@
-import Vue from 'vue';
-import Buefy from 'buefy';
-import 'buefy/lib/buefy.css';
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 import 'mdi/css/materialdesignicons.min.css'
-import './assets/css/style.css';
+import './assets/css/style.css'
 
-import App from './App.vue';
+import App from './App.vue'
 import router from './routes'
 
-Vue.use(Buefy);
+Vue.use(Buefy)
 
 new Vue({
-  el: '#app',
-  render: h => h(App),
   router,
-});
+  render: (h) => h(App),
+}).$mount('#app')
